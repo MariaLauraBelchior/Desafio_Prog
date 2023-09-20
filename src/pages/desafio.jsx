903dd2bf791f82000cb45d1a23bda0d4d9ffd3cd
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2023-09-20 01:20:04 -0300

    Prog Script

---
+++ src/pages/desafio.jsx
@@ -0,0 +1,69 @@
+export default function Desafio(){
+    return(
+        <div className="Desafio">
+        <div className="square">
+            <span className="piece white-piece">R</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">N</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">B</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">Q</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">K</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">B</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">N</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">R</span>
+        </div>
+        
+    
+        <div className="square">
+            <span className="piece white-piece">P</span>
+        </div>
+        <div className="square">
+            <span className="piece white-piece">P</span>
+        </div>
+
+        <div className="square">
+            <span className="piece black-piece">r</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">n</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">b</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">q</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">k</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">b</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">n</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">r</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">p</span>
+        </div>
+        <div className="square">
+            <span className="piece black-piece">p</span>
+        </div>
+        </div>
+    )
+}
